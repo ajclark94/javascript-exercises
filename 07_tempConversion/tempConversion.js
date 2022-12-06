01cc6ecf3b87359d3f0a74e4cfb721a6fa0d4d2c
Austin Clark <115447290+ajclark94@users.noreply.github.com>
2022-12-06 00:01:05 -0700

    Initial commit for leapYears and tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(temp) {
+  let degc = (temp - 32) * (5 / 9);
+  if (degc % 1 != 0) {
+    return Number(degc.toFixed(1));
+  } else {
+    return degc;
+  }
+  
+}; 
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(temp) {
+  let degf = (temp * (9 / 5)) + 32;
+  if (degf % 1 != 0) {
+    return Number(degf.toFixed(1));
+  } else {
+    return degf;
+  }
 };
 
 // Do not edit below this line
